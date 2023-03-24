5e6b423632ad1471c9a5d50720acbdf51e7a4fd6
musgravebrenner <musgravebrenner@gmail.com>
2023-03-24 01:20:00 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,4 +24,4 @@ streamlit.dataframe(fruits_to_show)
 #New section to display fruiotyvice api response
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
